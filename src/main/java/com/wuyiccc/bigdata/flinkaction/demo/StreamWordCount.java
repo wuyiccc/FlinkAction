3560c169ff7f7cc23f71a7ae537700e278c4e760
wuyiccc <3406324191@qq.com>
2021-12-08 23:12:22 +0800

    使用时间窗口处理流式数据

--- src/main/java/com/wuyiccc/bigdata/flinkaction/demo/StreamWordCount.java
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/demo/StreamWordCount.java
@@ -5,6 +5,7 @@ import com.wuyiccc.bigdata.utils.source.MySource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 /**
  * @author wuyiccc
@@ -15,10 +16,13 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         // 获取流处理的执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        SingleOutputStreamOperator<Tuple2<String, Integer>> result = env.addSource(new MySource()).flatMap(new Splitter()).keyBy(0).sum(1);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> result = env.addSource(new MySource())
+                .flatMap(new Splitter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(10))
+                .sum(1);
         result.print();
         // 流式数据时, flink是懒加载的, 所以此时必须调用execute方法才可以执行
         env.execute();
-
     }
 }
