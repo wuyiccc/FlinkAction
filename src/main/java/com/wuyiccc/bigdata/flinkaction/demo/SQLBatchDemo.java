d9e5006edc28ff6e849819fe6c0b6b0881725faa
wuyiccc <3406324191@qq.com>
2021-12-09 23:02:55 +0800

    使用SQL处理无界数据流和有界数据流

---
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/demo/SQLBatchDemo.java
@@ -0,0 +1,30 @@
+package com.wuyiccc.bigdata.flinkaction.demo;
+
+import com.wuyiccc.bigdata.pojo.MyOrder;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
+import org.apache.flink.types.Row;
+
+import static org.apache.flink.table.api.Expressions.$;
+
+/**
+ * @author wuyiccc
+ * @date 2021/12/9 22:51
+ */
+public class SQLBatchDemo {
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
+        DataSet<MyOrder> input = env.fromElements(
+                new MyOrder(1L, "BMW", 1),
+                new MyOrder(2L, "Tesla", 8),
+                new MyOrder(2L, "Tesla", 8),
+                new MyOrder(3L, "Rolls-Royce", 20));
+        tEnv.createTemporaryView("MyOrder", input, $("id"), $("product"), $("amount"));
+        Table table = tEnv.sqlQuery("SELECT product, SUM(amount) as amount FROM MyOrder GROUP BY product");
+        tEnv.toDataSet(table, Row.class).print();
+    }
+}
