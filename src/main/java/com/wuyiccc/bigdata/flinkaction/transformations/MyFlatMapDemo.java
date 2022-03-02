3ab121f59097f4aef37b1ce6f58e7a49ea7ce943
wuyiccc <3406324191@qq.com>
2022-03-03 00:52:25 +0800

    1. 使用FlatMap算子拆分子句

---
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/transformations/MyFlatMapDemo.java
@@ -0,0 +1,31 @@
+package com.wuyiccc.bigdata.flinkaction.transformations;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.util.Collector;
+
+import javax.sql.DataSource;
+
+/**
+ * @author wuyiccc
+ * @date 2022/3/3 0:48
+ * 使用FlatMap算子拆分句子
+ */
+public class MyFlatMapDemo {
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> dataSource = env.fromElements("Apache Flink is a framework and distributed processing engine for stateful computations over unbounded and bounded data streams");
+        FlatMapOperator<String, String> flatMap = dataSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String value, Collector<String> out) throws Exception {
+                for (String word : value.split(" ")) {
+                    out.collect(word);
+                }
+            }
+        });
+        flatMap.print();
+    }
+}
