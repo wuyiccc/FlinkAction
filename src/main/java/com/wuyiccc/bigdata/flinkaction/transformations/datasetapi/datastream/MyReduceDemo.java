acac46228284e517641e8a40e0ec687e3dd62a1b
wuyiccc <3406324191@qq.com>
2022-03-03 23:25:20 +0800

    使用Reduce操作键控流

---
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/transformations/datasetapi/datastream/MyReduceDemo.java
@@ -0,0 +1,36 @@
+package com.wuyiccc.bigdata.flinkaction.transformations.datasetapi.datastream;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author wuyiccc
+ * @date 2022/3/3 23:16
+ * 使用Reduce操作键控流
+ */
+public class MyReduceDemo {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment senv = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<Tuple2<String, Integer>> source = senv.fromElements(
+                new Tuple2<>("A", 1),
+                new Tuple2<>("B", 3),
+                new Tuple2<>("C", 6),
+                new Tuple2<>("A", 5),
+                new Tuple2<>("B", 8),
+                new Tuple2<>("B", 5)
+        );
+
+        DataStream<Tuple2<String, Integer>> reduce = source.keyBy(0).reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
+               return new Tuple2<>(value1.f0, value1.f1 + value2.f1);
+            }
+        });
+        reduce.print();
+        senv.execute("Reduce Demo");
+    }
+
+}
