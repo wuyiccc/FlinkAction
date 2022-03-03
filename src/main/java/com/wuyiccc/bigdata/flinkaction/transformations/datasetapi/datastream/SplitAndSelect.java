e1420a1aef627b9820cef3617af881d5db45b0ae
wuyiccc <3406324191@qq.com>
2022-03-03 23:36:26 +0800

    使用Split算子子拆分数据流, 并使用Select选择拆分后的数据流

---
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/transformations/datasetapi/datastream/SplitAndSelect.java
@@ -0,0 +1,42 @@
+package com.wuyiccc.bigdata.flinkaction.transformations.datasetapi.datastream;
+
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SplitStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wuyiccc
+ * @date 2022/3/3 23:28
+ * 使用Split算子子拆分数据流, 并使用Select选择拆分后的数据流
+ */
+public class SplitAndSelect {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment sEnv = StreamExecutionEnvironment.createLocalEnvironment();
+        DataStream<Integer> input = sEnv.fromElements(1, 2, 3, 4, 5, 6, 7, 8);
+        sEnv.setParallelism(1);
+        SplitStream<Integer> split = input.split(new OutputSelector<Integer>() {
+            @Override
+            public Iterable<String> select(Integer value) {
+                List<String> output = new ArrayList<>();
+                if (value % 2 == 0) {
+                    output.add("even");
+                } else {
+                    output.add("odd");
+                }
+                return output;
+            }
+        });
+        DataStream<Integer> even = split.select("even");
+        DataStream<Integer> odd = split.select("odd");
+        DataStream<Integer> all = split.select("even", "odd");
+        even.print("even流");
+        odd.print("odd流");
+        all.print("all流");
+        sEnv.execute();
+    }
+}
