691cb5df8fc839390c942b1922f9007d69316da3
wuyiccc <3406324191@qq.com>
2022-03-04 07:20:56 +0800

    读取和解析CSV文件

---
+++ src/main/java/com/wuyiccc/bigdata/flinkaction/dataset/source/ReadCSV.java
@@ -0,0 +1,23 @@
+package com.wuyiccc.bigdata.flinkaction.dataset.source;
+
+import com.wuyiccc.bigdata.flinkaction.dataset.User;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+/**
+ * @author wuyiccc
+ * @date 2022/3/4 7:02
+ * 读取和解析CSV文件
+ */
+public class ReadCSV {
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<User> inputData = env.readCsvFile("./file/test.csv")
+                .fieldDelimiter(",")
+                .ignoreFirstLine()
+                .includeFields(true, false, true)
+                .pojoType(User.class, "name", "age");
+        inputData.print();
+    }
+}
